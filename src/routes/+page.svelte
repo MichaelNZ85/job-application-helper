63733b38a307e4d2eb12006b8247747eb2a3f618
MichaelNZ85 <michaelnz10@outlook.com>
2024-05-06 22:14:48 +1200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import {Alert, Button} from 'flowbite-svelte';
+</script>
+
+<Alert color="green">
+    <span class="font-medium">Big purrs from the puddy!</span>
+</Alert>
+<Button color="purple">Purple</Button>
+  
+  <style lang="postcss">
+    :global(html) {
+      background-color: theme(colors.gray.400);
+    }
+  </style>
